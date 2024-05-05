4b6214a2e6ceb107dc9b746df02e2efbd1d93ea5
kumar-bibhuti <111742135+kumar-bibhuti@users.noreply.github.com>
2024-05-05 06:37:08 +0000

    conversionOperation

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "33" = 33
+// "3abc" = nan
+// true = 1 false = 0
+
+let isLoggedIn = "hi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false 
+// "bibhuti" = true
+
+let number = 22
+
+let stringNumber = String(number)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
