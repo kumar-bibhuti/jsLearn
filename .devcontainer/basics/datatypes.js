ba374c9de55f30e00712545dd912605a7bd723f0
kumar-bibhuti <111742135+kumar-bibhuti@users.noreply.github.com>
2024-05-05 06:11:30 +0000

    datatypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// alert (hello)
+
+//DataTypes
+
+//number => 2 to 53
+//bigint
+//string = ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "bibhuti@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 2
+accountEmail = "raushan@gmail.com"
+accountPassword = "3214"
+accountCity = "bihta"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
