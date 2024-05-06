d7cbc980fe9b0bae078a9056c405f2323762940f
kumar-bibhuti <111742135+kumar-bibhuti@users.noreply.github.com>
2024-05-06 15:00:20 +0000

    datatypessummary

---
+++ .devcontainer/basics/datatypessummary.js
@@ -0,0 +1,16 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, Bigint
+
+const data = ["ram", "mohan", "shyam"]
+console.log(typeof data);
+
+let myobject = {
+    "id" : 1,
+    "name" : "Bibhuti",
+}
+console.log(typeof myobject);
+
+// Refrence (Non primitive)
+
+// Array, Object, Funcitons
